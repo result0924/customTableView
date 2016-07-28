8b76fe48d5bb36fae14a0b46782cfe7d0bde55e2
result0924 <result0924@gmail.com>
2016-07-28 11:12:40 +0800

    Add new file and class

---
+++ customTableView/ActivityTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityTableViewCell.h
+//  customTableView
+//
+//  Created by justin on 2016/7/28.
+//  Copyright © 2016年 justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityTableViewCell : UITableViewCell
+
+@end
---
+++ customTableView/ActivityTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ActivityTableViewCell.m
+//  customTableView
+//
+//  Created by justin on 2016/7/28.
+//  Copyright © 2016年 justin. All rights reserved.
+//
+
+#import "ActivityTableViewCell.h"
+
+@implementation ActivityTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
