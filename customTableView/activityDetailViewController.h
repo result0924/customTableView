4d737407f39d47c2fc25911a5aaabd49b6169372
result0924 <result0924@gmail.com>
2016-07-29 14:47:36 +0800

    Add action detail view components

---
+++ customTableView/activityDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  activityDetailViewController.h
+//  customTableView
+//
+//  Created by justin on 2016/7/29.
+//  Copyright © 2016年 justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface activityDetailViewController : UIViewController
+
+@end
